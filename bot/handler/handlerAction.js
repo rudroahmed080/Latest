0c0fa8c5aef733ae82f706a196b459a24cf4052a
rudroahmed080 <rudroahmed080@gmail.com>
2025-04-09 10:34:26 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction ==  "😾"){
+        if(event.reaction ==  "❤️"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "61571904047861","100033670741301"){
     message.unsend(event.messageID)
 }else{
